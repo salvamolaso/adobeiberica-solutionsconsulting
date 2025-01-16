02b437d2bff07a70e6a820f91f3c9cbf69bcd0d6
salvamolaso <97615587+salvamolaso@users.noreply.github.com>
2025-01-16 13:41:47 +0100

    Update header.js
    Adding SignInModal

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -15,7 +15,6 @@ function collapseAllNavSections(sections) {
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
-
 export default async function decorate(block) {
   // fetch nav content
   const navMeta = getMetadata('nav');
@@ -63,10 +62,44 @@ export default async function decorate(block) {
     const topBar = document.createElement('div');
     topBar.classList.add('header-topbar');
     block.prepend(topBar);
-    topBar.innerHTML = '<div>Sign In</div><div class="header-markets"><span class="icon icon-flag-us"></span>EN-US<span class="header-chevron-down"></span></div>';
+    topBar.innerHTML = '<button id="signInButton">Sign In</button><div class="header-markets"><span class="icon icon-flag-us"></span>EN-US<span class="header-chevron-down"></span></div>';
     nav.prepend(hamburger);
+    const signInModal = document.createElement('div');
+    signInModal.classList.add('modal');
+    signInModal.id('signInModal');
+    signInModal.innerHTML = '<div class="modal-content"><span class="close">&times;</span><p>SometextintheModal..</p></div>';
+    nav.prepend(signInModal);
+    addSignInModalLogic();
     nav.setAttribute('aria-expanded', 'false');
     decorateIcons(block);
     block.append(nav);
   }
 }
+
+function addSignInModalLogic() {
+  // Get the modal
+  var signInModal = document.getElementById("signInModal");
+  // Get the button that opens the modal
+  var signInButton = document.getElementById("signInButton");
+  // Get the <span> element that closes the modal
+  var signInCloseSpan = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks the button, open the modal 
+  signInButton.onclick = function() {
+    signInModal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  signInCloseSpan.onclick = function() {
+    signInModal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == signInModal) {
+      signInModal.style.display = "none";
+    }
+  }
+}
+  
+

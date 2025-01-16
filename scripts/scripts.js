d5b77a9841ae71be0373615408ec4d7f1a589b38
salvamolaso <97615587+salvamolaso@users.noreply.github.com>
2025-01-16 12:40:18 +0100

    Update scripts.js
    Visible for authoring

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -301,7 +301,7 @@ function loadDelayed() {
 async function loadPage() {
   console.log('loading page');
   //Salva: Allowing AEM editing
-  if (document.location.host.startsWith("experience.adobe.com")) {
+  if (document.location.host.startsWith("author-")) {
     document.body.style.visibility = 'visible';
   }
   await window.hlx.plugins.load('eager');
